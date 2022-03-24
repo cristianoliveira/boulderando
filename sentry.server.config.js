f119985fbf44f3ab830755b03c55dd43506f4957
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-03-24 08:56:55 +0100

    chore: sentry integration using wisard

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: SENTRY_DSN || 'https://8f268c27885f4b80aae0ab09bfaf64b9@o1175165.ingest.sentry.io/6271843',
   tracesSampleRate: 1.0,
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
