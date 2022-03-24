2cf745600437536d3192c6520eb71d3828b55489
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-03-24 23:36:22 +0100

    fix: avoid using window on server

--- src/context/Environment/index.js
+++ src/context/Environment/index.js
@@ -3,18 +3,22 @@ import * as init from '../../api/bouldering-sessions'
 
 export const EnvironmentContext = createContext()
 
-export const EnvironmentConsumer = EnvironmentContext.Consumer;
+export const EnvironmentConsumer = EnvironmentContext.Consumer
 
 export function EnvironmentProvider({ children }) {
+  let hasDryRunParam = false
+  if (typeof window !== 'undefined') {
+    hasDryRunParam = window.location.search.includes('dry_run')
+  }
   const isDryRun =
     JSON.parse(`${process.env.NEXT_PUBLIC_DRY_RUN || 'false'}`) ||
-    window.location.search.includes('dry_run')
+    hasDryRunParam
 
-  const configs = { isDryRun, apiUrl: `${process.env.NEXT_PUBLIC_API_URL}`  }
+  const configs = { isDryRun, apiUrl: `${process.env.NEXT_PUBLIC_API_URL}` }
 
   const api = {
     getSessions: init.getSessions(configs),
-    postSessionSchedule: init.postSessionSchedule(configs)
+    postSessionSchedule: init.postSessionSchedule(configs),
   }
 
   return (
