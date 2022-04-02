a68b8a816adab6b27dfce03a5c3de49f80bda743
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-04-02 23:41:45 +0200

    feat: delete booked session history (#30)
    * feat: disabled button of booked sessions
    
    (cherry picked from commit 8d7fdfbd8659b69bfdb5f3f857959c4b5b9ab2c3)
    
    * feat: allows deleting booking history

--- src/components/BookingHistory/BookingList.js
+++ src/components/BookingHistory/BookingList.js
@@ -1,5 +1,6 @@
-import { Table, TableRow, TableCell, TableHead, Button } from '@mui/material'
+import { TableContainer, Table, TableRow, TableCell, TableHead, Button } from '@mui/material'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline'
 
 import useBookingHistoryContext from '../../context/BookingHistory'
 
@@ -21,11 +22,12 @@ ${result.gym_name} ${result.day_of_week} ${result.booking_date} ${result.schedul
     : ''
 
 export const BOOKING_HISTORY_LIST_ITEM = 'booking_history_list_item'
+export const BOOKING_HISTORY_DELETE_BTN = 'booking_history_delete_btn';
 
 function BookingList() {
-  const { bookingHistory } = useBookingHistoryContext()
+  const { bookingHistory, deleteBookedSession } = useBookingHistoryContext()
   return (
-    <>
+    <TableContainer>
       <Table>
         <TableHead>
           <TbCell>Gym</TbCell>
@@ -33,6 +35,7 @@ function BookingList() {
           <TbCell>Time</TbCell>
           <TbCell>Created At</TbCell>
           <TbCell>Copy</TbCell>
+          <TbCell>Delete</TbCell>
         </TableHead>
         {bookingHistory.map((session, i) => (
           <TableRow
@@ -59,10 +62,19 @@ function BookingList() {
                 />
               </Button>
             </TbCell>
+            <TbCell>
+              <Button data-testid={BOOKING_HISTORY_DELETE_BTN} color="inherit" size="small">
+                <DeleteOutlineIcon
+                  onClick={() => {
+                    deleteBookedSession(session)
+                  }}
+                />
+              </Button>
+            </TbCell>
           </TableRow>
         ))}
       </Table>
-    </>
+    </TableContainer>
   )
 }
 
