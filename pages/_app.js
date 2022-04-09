8a00e2703e2b9950541bf06793ff67280c985342
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-04-09 18:39:44 +0200

    feat: github fork it

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
-import { Typography, Link, Box } from '@mui/material'
+import { Link } from '@mui/material'
 import NavBar from '../src/components/NavBar'
 import DryRunBadge from '../src/components/DryRunBadge'
+import GithubForkIt from '../src/components/GithubForkIt'
 
 import {
   EnvironmentProvider,
@@ -20,21 +21,20 @@ function MyApp({ Component, pageProps }) {
           name="description"
           content="Booking bouldering sessions made easy"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <Link rel="icon" href="/favicon.ico" />
       </Head>
 
       <EnvironmentProvider>
         <EnvironmentConsumer>
           {({ configs, api }) => (
             <>
-              <DryRunBadge
-                isVisible={configs.isDryRun}
-              />
+              <DryRunBadge isVisible={configs.isDryRun} />
               <UserProvider>
                 <SessionProvider api={api}>
                   <BookingHistoryProvider>
                     <NavBar />
                     <Component {...pageProps} />
+                    <GithubForkIt />
                   </BookingHistoryProvider>
                 </SessionProvider>
               </UserProvider>
@@ -42,21 +42,6 @@ function MyApp({ Component, pageProps }) {
           )}
         </EnvironmentConsumer>
       </EnvironmentProvider>
-
-      <Box>
-        <Typography variant="small" color="text.primary" align="center">
-          <small>Developed by Bouldering Fans. </small>
-          <Link
-            href="https://github.com/cristianoliveira/boulderando"
-            target="_blank"
-            rel="noopener noreferrer"
-            align="center"
-            variant="body2"
-          >
-            Fork it on Github
-          </Link>
-        </Typography>
-      </Box>
     </>
   )
 }
