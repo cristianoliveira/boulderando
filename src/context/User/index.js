34235187ca6ccdc14e1dc8db76b799e53154dca7
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-05-01 09:56:46 +0200

    feat: redirect back to bot after user created

--- src/context/User/index.js
+++ src/context/User/index.js
@@ -2,35 +2,49 @@ import { createContext, useContext, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import useStorage from '../../hooks/useStorage'
-import { USER } from '../../storage/items';
+import { USER } from '../../storage/items'
+import { getRedirectParam, browserRedirectTo } from '../../modules/redirect-to'
 
 export const UserContext = createContext()
 
 export const UserConsumer = UserContext.Consumer
+
+const URI_USER_NEW = '/user/new'
+const URI_USER_EDIT = '/user/edit'
 
 export function UserProvider({ children, api }) {
   const router = useRouter()
   const [user, setUser, removeUser] = useStorage(USER)
 
   const editUser = () => {
-    router.push('/user/edit')
+    router.push(URI_USER_EDIT)
   }
 
   const deleteUser = () => {
     removeUser()
-    router.push('/user/new')
+    router.push(URI_USER_NEW)
   }
 
   const saveUser = (userToSave) => {
     api.postCreateUser(userToSave).then(() => {
       setUser(userToSave)
+
+      const redirectTo = getRedirectParam()
+      if (redirectTo) {
+        browserRedirectTo(redirectTo)
+      }
+
       router.push('/')
     })
   }
 
   useEffect(() => {
-    if (!user && !router.pathname.includes('sync')) {
-      router.push('/user/new')
+    if (
+      !user &&
+      !router.pathname.includes('sync') &&
+      !router.pathname.includes(URI_USER_NEW)
+    ) {
+      router.push(URI_USER_NEW)
     }
   }, [user])
 
