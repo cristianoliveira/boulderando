207164864c9c1293837d110c14a4a0496a50a2b9
Cristian Oliveira <contato@cristianoliveira.com.br>
2022-05-15 23:42:12 +0200

    refact: session context to ts

--- src/context/Sessions/index.tsx
+++ src/context/Sessions/index.tsx
@@ -2,44 +2,58 @@ import { createContext, useContext, useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
 import useListStorage from '../../hooks/useListStorage'
-import { SESSIONS } from '../../storage/items';
+import { SESSIONS } from '../../storage/items'
 
-export const SessionContext = createContext()
+type SessionContextValue = {
+  sessions: StorageData<Session>
+  deleteCustomSessions(): void
+  scheduleSession(
+    session: Session,
+    user: User
+  ): Promise<{ data?: object; error?: object }>
+  saveCustomSession(session: Session): void
+  isProcessing: boolean
+  hasSubmitted: boolean
+} | null
+
+export const SessionContext = createContext<SessionContextValue>(null)
 
 export const SessionConsumer = SessionContext.Consumer
 
-export function SessionProvider({ children, api }) {
+export function SessionProvider({ children, api }: WithChildren & WithApi) {
   const [hasSubmitted, setHasSubmitted] = useState(false)
   const [isProcessing, setIsProcessing] = useState(false)
   const [sessions, setSessions, resetSessions] = useListStorage(SESSIONS, [])
   const router = useRouter()
 
-  const scheduleSession = (session, user) => {
+  const scheduleSession = (session: Session, user: User) => {
     setHasSubmitted(true)
     setIsProcessing(true)
-    return api.postSessionSchedule(user, session).then(({ data, error }) => {
-      setIsProcessing(false)
+    return api
+      .postSessionSchedule(user, session)
+      .then(({ data, error }: any) => {
+        setIsProcessing(false)
 
-      if (data) {
-        return { data: { ...data, ...session } };
-      }
+        if (data) {
+          return { data: { ...data, ...session } }
+        }
 
-      return { error }
-    })
+        return { error }
+      })
   }
 
   const deleteCustomSessions = () => {
     resetSessions()
   }
 
-  const saveCustomSession = (session) => {
+  const saveCustomSession = (session: Session) => {
     setSessions([...sessions, session])
     router.push('/sessions')
   }
 
   useEffect(() => {
     if (!sessions.length) {
-      api.getSessions().then((defaultSessions) => {
+      api.getSessions().then((defaultSessions: Session[]) => {
         setSessions(defaultSessions)
       })
     }
--- src/types/storage.d.ts
+++ src/types/storage.d.ts
@@ -1,6 +1,12 @@
-declare type StorageData = string | number | object | null | StorageData[];
+declare type StorageData<T> =
+  | T
+  | string
+  | number
+  | object
+  | null
+  | StorageData<T>[]
 
-type StorageKey = string;
+type StorageKey = string
 
 type SaveStorageItem = (item: StorageData) => void
 type RemoveStorageItem = () => void
